6ce4c94701d9a433538c5ce71d69837b8e12df4f
Cullen REDDY <cull.redd@gmail.com>
2022-01-01 13:56:03 -0800

    switching over from GHE

--- js/main.js
+++ js/main.js
@@ -1,1 +1,29 @@
-console.log("👀")
+const url ="https://api.kanye.rest";
+
+const yeRequest = () => {
+    fetch(url).then((response) => {
+        response.json().then((data) => {
+            const wizdomNugget = document.createElement('p');
+            console.log(data);
+            wizdomNugget.innerText = data['quote'];
+            console.log(wizdomNugget);
+            document.getElementById('container').append(wizdomNugget);
+        })
+    })
+}
+
+const button = document.getElementById('btn');
+button.addEventListener('click', yeRequest);
+
+// stretch goals: add other API functionality
+// const url2 = "https://icanhazdadjoke.com/api";
+// const presser = document.getElementById('dad');
+// presser.addEventListener('click', dadJokes);
+
+// function dadJokes() {
+//     fetch(url2).then((response) => {
+//         response.json().then((data) => {
+            
+//         })
+//     })
+// }
